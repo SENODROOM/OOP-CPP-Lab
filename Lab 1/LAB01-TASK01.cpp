74b69752a58a97fc4ac7ec5777db2425abbd925a
SENODROOM <136210526+SENODROOM@users.noreply.github.com>
2026-05-20 10:19:53 +0500

    lab 01

--- Lab 1/LAB01-TASK01.cpp
+++ Lab 1/LAB01-TASK01.cpp
@@ -16,5 +16,8 @@ int main()
 	cout << "Memory address a: " << ptra << endl;
 	cout << "Memory address b: " << ptrb << endl;
 	cout << "Memory address c: " << ptrc << endl;
+	cout << "Value of a: " << *ptra << endl;
+	cout << "Value of b: " << *ptrb << endl;
+	cout << "Value of c: " << *ptrc << endl;
 	return 0;
 }
